a349fc1371a2f3a0137ad036cc3d92070db39eed
RainRahma <dengdau3@gmail.com>
2022-08-25 12:13:23 +0300

    Dashboard
    A responsive Dashboard in HTML, CSS and JavaScript

---
+++ orders.js
@@ -0,0 +1,56 @@
+const Orders= [
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '436675',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Canon Camera',
+        ProductNumber: '324675',
+        paymentStatus: 'Paid',
+        shipping: 'On Transit'
+    },
+    {
+        productName: 'Hp Pc',
+        ProductNumber: '87647378',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Iphone 11 pro Max 64',
+        ProductNumber: '864356',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Dell Desktop',
+        ProductNumber: '764374',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Cross Pendant Chain',
+        ProductNumber: '438975',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }, 
+    {
+        productName: 'Camera Flashlight',
+        ProductNumber: '986675',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Military Gideon Boots',
+        ProductNumber: '886675',
+        paymentStatus: 'Not Paid',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'HomePod',
+        ProductNumber: '97475',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
